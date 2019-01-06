66376af0622ac52f4946451e8320cf594966a5f5
1138-4EB <1138-4EB@users.noreply.github.com>
2019-01-06 01:25:38 +0100

    wip

--- build.sh
+++ build.sh
@@ -2,15 +2,8 @@
 
 set -e
 
-# Check if filebrowser exists but is empty (i.e., we are inside filebrowser/dev)
-if [[ ! -d ../filebrowser/.git ]]; then
-  cd ..
-  rm -rf filebrowser
-  cd caddy
-fi
-
 go get -v ./...
-cd ../../mholt/caddy/caddy
+cd /go/src/github.com/mholt/caddy/caddy
 go get -v ./...
 go get -v github.com/caddyserver/builds
 sed -i.bak 's#// This is where other plugins get plugged in (imported)#_ "github.com/filebrowser/caddy"#' caddymain/run.go
