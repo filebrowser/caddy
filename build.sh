a99f841377efb9bb8e78e0af5375d0fbb90ddd20
Henrique Dias <hacdias@gmail.com>
2019-05-08 23:01:30 +0100

    ...
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-set -e
+set -euo pipefail
 
-go get -v ./...
-cd /go/src/github.com/mholt/caddy/caddy
-go get -v ./...
-go get -v github.com/caddyserver/builds
+git clone https://github.com/mholt/caddy
+cd caddy/caddy
 sed -i.bak 's#// This is where other plugins get plugged in (imported)#_ "github.com/filebrowser/caddy"#' caddymain/run.go
+go get
 go run build.go
 ls -la
